0bfac05334515783e2dfeb5a72998a4e0d730955
DostonNazarov <dostondev4477@gmail.com>
2024-02-29 15:57:44 +0500

    Add project files.

---
+++ BlogWebsite/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlogWebMVC.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public List<Post>? Posts { get; set; }
+    }
+}
