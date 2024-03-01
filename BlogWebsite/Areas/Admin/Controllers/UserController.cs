e4280bc0415d0d1aebc85e5671a41bf8f99cd349
DostonNazarov <dostondev4477@gmail.com>
2024-03-01 10:38:29 +0500

    login theme added

--- BlogWebsite/Areas/Admin/Controllers/UserController.cs
+++ BlogWebsite/Areas/Admin/Controllers/UserController.cs
@@ -5,9 +5,19 @@ namespace BlogWebsite.Areas.Admin.Controllers
     [Area("Admin")]
     public class UserController : Controller
     {
+
+     
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+       
     }
 }
