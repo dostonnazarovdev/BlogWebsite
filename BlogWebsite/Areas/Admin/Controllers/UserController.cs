7afa285ed4475de7030b46dfd26462bb53a36278
DostonNazarov <dostondev4477@gmail.com>
2024-03-02 19:58:33 +0500

    added update

--- BlogWebsite/Areas/Admin/Controllers/UserController.cs
+++ BlogWebsite/Areas/Admin/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
 using BlogWebsite.Models;
+using BlogWebsite.Utilites;
 using BlogWebsite.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -10,7 +11,7 @@ namespace BlogWebsite.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UserController : Controller
-    {
+    { 
 
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
@@ -24,11 +25,68 @@ namespace BlogWebsite.Areas.Admin.Controllers
             _notifySerivce = notyfService;
         }
 
-        [Authorize(Roles ="Admin")]
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> Registor(RegisterVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var checkByEmail =await _userManager.FindByEmailAsync(vm.Email);
+            if (checkByEmail != null)
+            {
+                _notifySerivce.Error("Email already exist!");
+                return View(vm);
+            }
+
+            var checkByUsername = await _userManager.FindByNameAsync(vm.UserName);
+            if (checkByUsername != null)
+            {
+                _notifySerivce.Error("Username already exist!");
+                return View(vm);
+            }
+
+            var applicationUser = new ApplicationUser()
+            {
+                Email = vm.Email,
+                UserName = vm.UserName,
+                FirstName = vm.FirstName,
+                LastName = vm.LastName
+            };
+            var checkUser = await _userManager.CreateAsync(applicationUser, vm.Password);
+            if (checkUser.Succeeded)
+            {
+                if (vm.IsAdmin)
+                {
+                    await _userManager.AddToRoleAsync(applicationUser, WebsiteRoles.WebsiteAdmin);
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(applicationUser, WebsiteRoles.WebsiteAuthor);
+                }
+                _notifySerivce.Success("User registered successfully");
+                return RedirectToAction("Index", "User", new { area = "Admin" });
+            }
+           
+            return View(vm);
+        }
+
+
+        [Authorize(Roles = "Admin")]
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var users =await _userManager.Users.ToListAsync();
+            var users = await _userManager.Users.ToListAsync();
             var vm = users.Select(x => new UserVM()
             {
                 Id = x.Id,
@@ -39,15 +97,18 @@ namespace BlogWebsite.Areas.Admin.Controllers
             return View(vm);
         }
 
+
         [HttpGet("Login")]
         public IActionResult Login()
         {
             if (!HttpContext.User.Identity!.IsAuthenticated)
             {
-            return View(new LoginVM());
+                return View(new LoginVM());
             }
-            return RedirectToAction("Index", "User", new {Areas="Admin"});
+            return RedirectToAction("Index", "User", new { Areas = "Admin" });
         }
+
+
         [HttpPost("Login")]
         public async Task<IActionResult> Login(LoginVM vm)
         {
@@ -79,7 +140,7 @@ namespace BlogWebsite.Areas.Admin.Controllers
         {
             _signInManager.SignOutAsync();
             _notifySerivce.Success("You are logged out successfull");
-            return RedirectToAction("Index", "Home", new {area=""});
+            return RedirectToAction("Index", "Home", new { area = "" });
         }
     }
 }
