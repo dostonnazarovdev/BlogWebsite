519a394492ae8996c29580acd403465a9f5f3c9a
DostonNazarov <dostondev4477@gmail.com>
2024-05-04 11:20:52 +0430

    post model added

---
+++ BlogWebsite/Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using BlogWebsite.Data;
+using BlogWebsite.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogWebsite.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notyfService { get; }
+
+        public BlogController(ApplicationDbContext context,INotyfService notyfService)
+        {
+            _context = context;
+            _notyfService = notyfService;
+        }
+        [HttpGet]
+        public IActionResult Post(string slug)
+        {
+            if (slug == "")
+            {
+                _notyfService.Error("Post not found!");
+                 return View();
+            }
+            var post = _context.Posts!.Include(x=>x.ApplicationUser).FirstOrDefault(a=>a.Slug==slug);
+            if (post == null)
+            {
+                _notyfService.Error("Post not found!");
+                return View();
+            }
+
+            var vm = new BlogPostVM()
+            {
+                Id= post.Id,
+                Title=post.Title,
+                AuthorName=post.ApplicationUser!.LastName+" "+post.ApplicationUser.FirstName,
+                CreateDate=post.CreatedDate,
+                PotoUrl=post.PhotoUrl,
+                Description=post.Description,
+                ShortDescription=post.ShortDescription
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ BlogWebsite/ViewModels/BlogPostVM.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogWebsite.ViewModels
+{
+    public class BlogPostVM
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? AuthorName { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string? PotoUrl { get; set; }
+        public string? ShortDescription {  get; set; }
+        public string? Description { get; set; }
+    }
+}
